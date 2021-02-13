d79c79219b1394e74f27ad8e6f98d3dfceb56dc2
Victor Freitas <60153670+cnfreitax@users.noreply.github.com>
2021-02-13 19:06:40 -0300

    refactor: remove questions field and add type from interface

--- src/tests/presentation/mocks/fake-request-add-exame.ts
+++ src/tests/presentation/mocks/fake-request-add-exame.ts
@@ -1,28 +1,11 @@
-import { HttpRequest } from 'shared/interfaces';
+import { ProofType } from '../../../domain/models';
+import { HttpRequest } from '../../../shared/interfaces';
 
 export const mockFakeRequest = (): HttpRequest => ({
   body: {
     id: 'any_exam_id',
     name: 'any_exam',
     description: 'any_description',
-    type: 'any_type',
-    questions: [
-      {
-        id: 'any_question_id',
-        statement: 'any_statement',
-        options: [
-          {
-            id: 'any_option_id',
-            value: 'viver',
-            correct: false,
-          },
-          {
-            id: 'another_option_id',
-            value: 'viver',
-            correct: false,
-          },
-        ],
-      },
-    ],
+    type: ProofType.ONLINE,
   },
 });
